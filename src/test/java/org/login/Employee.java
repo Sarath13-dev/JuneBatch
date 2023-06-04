b182b0ed7b57fe4f5a6379e658b9123079343666
Sarath13-dev <ramsssjrs@gmail.com>
2023-06-04 15:02:19 +0530

    first commit

---
+++ src/test/java/org/login/Employee.java
@@ -0,0 +1,19 @@
+package org.login;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Employee {
+	
+	@Test
+	private void tc0() {
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://www.facebook.com/");
+	}
+
+}
